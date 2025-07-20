4bf7cd38e2af04dbeae9f9705852d95eefb5f6da
riteshum21 <ritesh.shukl8085@gmail.com>
2025-07-21 00:39:15 +0530

    rg added

--- Module/main.tf
+++ Module/main.tf
@@ -3,6 +3,12 @@ module "rg" {
   rg = "riteshrg"
   location = "West Europe"
 }
+# new rg added
+module "rg1" {
+  source = "../Child/RG"
+  rg = "riteshrg1"
+  location = "West Europe"
+}
 
 module "stg" {
   source = "../Child/STG"
